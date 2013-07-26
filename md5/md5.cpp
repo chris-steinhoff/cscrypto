311fb53c27986627fb2de2818f579eb9b293edef
Chris Steinhoff <cs.otherstuff@gmail.com>
2013-07-26 21:35:21 +0000

    Separate rounds for less conditionals.

--- md5/md5.cpp
+++ md5/md5.cpp
@@ -1,6 +1,4 @@
 
-#include <iostream>
-#include <stdio.h>
 #include <string.h>
 #include "md5.h"
 
@@ -52,79 +50,7 @@ void Md5::update(const ui8* const data, size_t len) {
 			available = CHUNK_SIZE;
 		}
 	}
-
-
-	// Represent buffer as 16 32-bit words
-	/*ui32 words[16] = {
-		bytes_to_int(buffer), bytes_to_int(&buffer[4]),
-		bytes_to_int(&buffer[8]), bytes_to_int(&buffer[12]),
-		bytes_to_int(&buffer[16]), bytes_to_int(&buffer[20]),
-		bytes_to_int(&buffer[24]), bytes_to_int(&buffer[28]),
-		bytes_to_int(&buffer[32]), bytes_to_int(&buffer[36]),
-		bytes_to_int(&buffer[40]), bytes_to_int(&buffer[44]),
-		bytes_to_int(&buffer[48]), bytes_to_int(&buffer[52]),
-		bytes_to_int(&buffer[56]), bytes_to_int(&buffer[60])
-	};
-
-	// Get the current state to manipulate
-	ui32 a = A, b = B, c = C, d = D;
-	ui32 f, g, z, tmp;
-	size_t i;
-
-#define ROTATION \
-tmp = d;\
-d = c;\
-c = b;\
-z = (a + f + salts[i] + words[g]);\
-b = b + ((z << shifts[i]) | (z >> (32-shifts[i])));\
-a = tmp;
-
-	// First round
-	for( ; i < 16 ; ++i) {
-		f = (b & c) | ((!b) & d);
-		g = i;
-		ROTATION
-	}
-
-	// Second round
-	for( ; i < 32 ; ++i) {
-		f = (d & b) | ((~b) & c);
-		g = (5 * i + 1) % 16;
-		ROTATION
-	}
-
-	// Third round
-	for( ; i < 32 ; ++i) {
-		f = b ^ c ^ d;
-		g = (3 * i + 5) % 16;
-		ROTATION
-	}
-
-	// Forth round
-	for( ; i < 32 ; ++i) {
-		f = c ^ (b | (~d));
-		g = (7 * i) % 16;
-		ROTATION
-	}
-
-	// Add to the hash
-	A += a;
-	B += b;
-	C += c;
-	D += d;
-
-	// If there is more data then what remained in the buffer,
-	// add the left over to the buffer
-	if(len > remaining) {
-		// Copy left over data
-		size_t left_over = len - remaining;
-		memcpy(buffer, &data[start + remaining], left_over);
-		// Update the buffer pointer
-		buffer_index = left_over;
-	}*/
 }
-
-#define LEFTROTATE(x, c) (((x) << (c)) | ((x) >> (32 - (c))))
 
 void Md5::finish(ui8 digest[16]) {
 	// Append the 0x80 terminator
@@ -161,40 +87,59 @@ void Md5::finish(ui8 digest[16]) {
 }
 
 void Md5::hash_buffer() {
-	//std::cout << "Hashing buffer: " << buffer << std::endl;
-	ui32 words[16];
-	ui32 a = A;
-	ui32 b = B;
-	ui32 c = C;
-	ui32 d = D;
+	ui32 a = A, b = B, c = C, d = D;
+	ui32 f, g, z, temp;
+	unsigned char i = 0;
 
-	for(char i = 0 ; i < 16 ; ++i) {
-		words[i] = bytes_to_int(buffer + i * 4);
+	// Represent buffer as 16 32-bit words
+	const ui32 words[16] = {
+		bytes_to_int(buffer), bytes_to_int(&buffer[4]),
+		bytes_to_int(&buffer[8]), bytes_to_int(&buffer[12]),
+		bytes_to_int(&buffer[16]), bytes_to_int(&buffer[20]),
+		bytes_to_int(&buffer[24]), bytes_to_int(&buffer[28]),
+		bytes_to_int(&buffer[32]), bytes_to_int(&buffer[36]),
+		bytes_to_int(&buffer[40]), bytes_to_int(&buffer[44]),
+		bytes_to_int(&buffer[48]), bytes_to_int(&buffer[52]),
+		bytes_to_int(&buffer[56]), bytes_to_int(&buffer[60])
+	};
+
+#define ROTATION \
+temp = d;\
+d = c;\
+c = b;\
+z = (a + f + salts[i] + words[g]);\
+b = b + ((z << shifts[i]) | (z >> (32-shifts[i])));\
+a = temp;
+
+	// First round
+	for( ; i < 16 ; ++i) {
+		f = (b & c) | ((~b) & d);
+		g = i;
+		ROTATION
 	}
 
-	for(int i = 0 ; i < 64 ; ++i) {
-		ui32 f, g, temp;
-		if(i < 16) {
-			f = (b & c) | ((~b) & d);
-			g = i;
-		} else if(i < 32) {
-			f = (d & b) | ((~d) & c);
-			g = (5 * i + 1) % 16;
-		} else if(i < 48) {
-			f = b ^ c ^ d;
-			g = (3 * i + 5) % 16;
-		} else {
-			f = c ^ (b | (~d));
-			g = (7 * i) % 16;
-		}
-
-		temp = d;
-		d = c;
-		c = b;
-		b = b + LEFTROTATE((a + f + salts[i] + words[g]), shifts[i]);
-		a = temp;
+	// Second round
+	for( ; i < 32 ; ++i) {
+		f = (d & b) | ((~d) & c);
+		g = (5 * i + 1) % 16;
+		ROTATION
 	}
 
+	// Third round
+	for( ; i < 48 ; ++i) {
+		f = b ^ c ^ d;
+		g = (3 * i + 5) % 16;
+		ROTATION
+	}
+
+	// Forth round
+	for( ; i < 64 ; ++i) {
+		f = c ^ (b | (~d));
+		g = (7 * i) % 16;
+		ROTATION
+	}
+
+	// Update hash state
 	A += a;
 	B += b;
 	C += c;
