1c1c98322da750fd02653a6e325f47d95685acf7
Chris Steinhoff <cs.otherstuff@gmail.com>
2013-08-09 21:38:37 -0600

    Add SseMd5 to benchmarking.

--- md5/benchmark_md5.cpp
+++ md5/benchmark_md5.cpp
@@ -1,17 +1,20 @@
 #include <fstream>
 #include <hayai.hpp>
 #include "basic_md5.h"
+#include "sse_md5.h"
 
 #define DATA_SIZE 1000
 
 class Md5Fixture : public Hayai::Fixture {
 public:
 	Md5* basic;
-	char* data;//[DATA_SIZE];
-	ui8* digest;//[DIGEST_SIZE];
+	Md5* sse;
+	char* data;
+	ui8* digest;
 
 	virtual void SetUp() {
 		this->basic = new BasicMd5;
+		this->sse = new SseMd5;
 		this->data = new char[DATA_SIZE];
 		this->digest = new ui8[DIGEST_SIZE];
 
@@ -23,8 +26,9 @@ public:
 
 	virtual void TearDown() {
 		delete this->basic;
-		delete this->data;
-		delete this->digest;
+		delete this->sse;
+		delete[] this->data;
+		delete[] this->digest;
 	}
 };
 
@@ -35,6 +39,15 @@ BENCHMARK_F(Md5Fixture, BasicMd5, 100, 1000) {
 	basic->update((ui8*)data, DATA_SIZE);
 	basic->update((ui8*)data, DATA_SIZE);
 	basic->finish(digest);
+}
+
+BENCHMARK_F(Md5Fixture, SseMd5, 100, 1000) {
+	sse->update((ui8*)data, DATA_SIZE);
+	sse->update((ui8*)data, DATA_SIZE);
+	sse->update((ui8*)data, DATA_SIZE);
+	sse->update((ui8*)data, DATA_SIZE);
+	sse->update((ui8*)data, DATA_SIZE);
+	sse->finish(digest);
 }
 
 int main() {
